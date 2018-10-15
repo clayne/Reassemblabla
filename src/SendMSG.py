1da6de48aba0ebb3c29a1ad37080aeffe48c36f9
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2018-10-15 01:55:32 -0400

    Add Telegram feature

--- src/SendMSG.py
+++ src/SendMSG.py
@@ -10,6 +10,17 @@ from optparse import OptionParser
 import binascii 
 from global_variables import *
 import requests
+import telegram
 
-target_url = 'https://maker.ifttt.com/trigger/TestDone/with/key/cBDnuINFgFT0GIw6ajAAwB'
-r = requests.post(target_url, data={"value1" : "[Coreutils Performance Test]"})
+def SendMessage(msgstr):
+	target_url = 'https://maker.ifttt.com/trigger/TestDone/with/key/cBDnuINFgFT0GIw6ajAAwB'
+	r = requests.post(target_url, data={"value1" : msgstr})
+
+def SendTelegram(msgstr):
+	my_token = '686723033:AAGcsOMwDZAjsRwrsSK2gQRUNiHpn3hZQSw' # BotFather가 준 HTTP API입력
+	bot = telegram.Bot(token = my_token)   # bot 선언.
+	updates = bot.getUpdates()  # 업데이트
+	chat_id = bot.getUpdates()[-1].message.chat.id
+	bot.sendMessage(chat_id = chat_id, text=msgstr)
+
+SendTelegram("잘되냐?")
