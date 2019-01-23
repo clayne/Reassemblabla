749c4bec975df666b994f85961ce6c0ca3badff6
Jiwon Choi <jiwon.choi@kaist.ac.kr>
2019-01-23 13:37:59 +0900

    edited vsa function for symbolize only memory reference value

--- src/vsa.py
+++ src/vsa.py
@@ -11,73 +11,52 @@ from optparse import OptionParser
 import binascii 
 
 from etc import *
-'''
-89 /r	MOV r/m32,r32
-8B /r	MOV r32,r/m32
-C7 /0	MOV r/m32,imm32
-'''
-# 우선,, 자료구조는 어떻게 만들까? 
 
-
-def backward_slice(resdic, SectionName, ADDR): # resdic[SectionName][ADDR] 를 입력하면은 ADDR 바로 전 주소를 리턴해준당 ㅎㅅㅎ
-	addrlist = resdic[SectionName].keys()
-	addrlist = sorted(addrlist)
-	for i in xrange(len(addrlist)):
-		if addrlist[i] is ADDR:
-			return addrlist[i-1]
-	return -1
-
-# TODO: 일시적으로 비활성해줌. 왜냐하면 date에 
-'''
-	addl $0x805a953, %eax
-	#=> ADDR:0x80494df BYTE:0553a90508
-	이런게 있기때문임...
-'''
-
-# TODO: 내가 하고자하는게 : 해당라인에있는 hex값이 memory reference라면 true리턴, 아니라면 false리턴. 
-#         그러면 아래 date사태는 어케해결하냐? 
-#         "무에서유창조" 인스트럭션들(아무것도없는 황무지상태의 reg에 한방울 단비를 내리는 인스트럭션) ex) lea, mov, xor eax,eax 등등
-#         ㄴ 이것도 잠자는 에뮬레이션모듈의 코털을 건드리도록 하자. 
-def VSA_is_memoryAddr_ornot(DISASM): 
-	INSTRUCTION_CONTAINING_MEMREF = []
-	INSTRUCTION_CONTAINING_MEMREF.append(' call')
-	INSTRUCTION_CONTAINING_MEMREF.append(' clflush')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	INSTRUCTION_CONTAINING_MEMREF.append(' ')
-	# resdic[SectionName][ADDR][1] 중에서 original line 만을 pick한다. 
-	# 왜냐면 VSA에는 내가 추가한 라인이아니라, 오로지 원본바이너리의 원본라인만을 취하여 VSA해야하기 때문이다. 
-
+def VSA_and_extract_addr(DISASM):
+	'''
+	- extract every hex value from 1 DISASM
+	- ex)
+		mov    $0x20804,%al              --> []
+		je     804841b <frame_dummy+0xb> --> [804841b] 
+		push   $0x8048540                --> []
+	'''
 	
-	# call, jmp (*)
+	addrlist = []
+	origDISASM = DISASM
 
-	return True
+	if '#' in DISASM:		
+		DISASM = DISASM[:DISASM.index('#')]											# 1. 주석날리고
+	DISASM = re.sub('(0x)?' + '[0-9a-f]+' + '(\()' + '.*?' + '(\))', '', DISASM)	# 2. 메모리레퍼런스 0x12(%eax, %ebx, 4) 날리고
+	DISASM = DISASM.replace(',',' ') 												# 3. 콤마날리고 
+	DISASM = re.sub('\s+',' ',DISASM).strip() 										# 4. duplicate space, tab --> single space
+	DISASM = DISASM.split(' ') 
+
+	if len(DISASM) > 1:
+		for i in xrange(len(DISASM)):
+			OPCODE = DISASM[0]
+			if OPCODE.startswith('.'):  					# STOP-1. Assembler directive 인 경우 스톱.
+				break
+			if OPCODE.startswith('lea'): 					# STOP-2. LEA(메모리값이긴하지만, 메모리참조연산은 아니다.)
+				break
+
+			if i == 0: 										# SKIP-1. 인스트럭션이라면(DISASM[0]) 넘겨라
+				continue
+			elif len(DISASM[i]) == 0: 						# SKIP-2. 쓸모없는거라면 넘겨라
+				continue
+			elif DISASM[i].startswith('%'): 				# SKIP-3. 레지스터라면.. 넘겨라
+				continue
+			elif DISASM[i].startswith('-'): 				# SKIP-4. 음수라면 넘겨라! 쓸데가리없음.
+				continue
+			elif DISASM[i].startswith('$'): 				# SKIP-5. 중요!!! IMM값이라면!! 넘겨라...
+				continue
 	
+			else:
+				DISASM[i] = DISASM[i].replace('0x','')
+				DISASM[i] = DISASM[i].replace('*', '') 
+				if DISASM[i] == '': # 전처리 후 나온 DISASM 이 '' 이라면 패스 
+					continue  
+				if ishex(DISASM[i]): 
+					addrlist.append(int('0x'+DISASM[i],16)) 
+
+	return addrlist
+		
